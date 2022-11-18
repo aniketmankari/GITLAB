920519e9bb0519d35108393d32ddedd55cd6f6aa
aniketmankari <mankarianiket>
2022-11-18 16:37:04 +0530

    done

---
+++ hello.java
@@ -0,0 +1,17 @@
+public class hello{
+
+public static void main (String[] args){
+
+System.out.println("Hello World!");
+
+System.out.println("Hello World! Welcome to jenkins.");
+
+System.out.println("Hello World!");
+System.out.println("Hello World!");
+System.out.println("REFLECTED CHANGES");
+System.out.println("new branch b1 is created and changes are pushed to remote ");
+
+
+
+
+}}
